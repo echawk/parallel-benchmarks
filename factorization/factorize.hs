cd33d457613e7398dd4113cfaf638f241fab6cfc
Ethan <ethan.hawk@valpo.edu>
2022-05-25 15:33:57 -0500

    factorize.hs: refactor & add -O3

--- factorization/factorize.hs
+++ factorization/factorize.hs
@@ -1,21 +1,13 @@
 import Control.Concurrent
 
 -- To compile:
--- # ghc -threaded factorize.hs
+-- # ghc -O3 -threaded factorize.hs
 
 -- To run:
 -- # ./factorize +RTS -N<num_threads>
 
 factorize :: Int -> [Int]
 factorize n = filter (\x -> n `mod` x == 0) [1..n]
-
-print_factors :: Int -> IO ()
-print_factors n =
-  print $! factorize n
-
-print_factorize_list :: [Int] -> IO ()
-print_factorize_list l = do
-  mapM_ print_factors l
 
 -- For multi threading with 8 threads, we need to
 -- split up the infinite list into 8 infinite lists...
@@ -29,23 +21,19 @@ print_factorize_list l = do
 -- [2,5..]
 -- [3,6..]
 
-h :: (Int, Int) -> [Int]
-h (o,t) = [o,t..]
-
 g :: Int -> [[Int]]
 g n =
   let l = [1..n*2] in
-  map h $ zip (take n l) (drop n l)
+  map (\(a,b) -> [a,b..]) $ zip (take n l) (drop n l)
 
-thread_print_factorize_list :: Chan () -> [Int] -> IO ()
-thread_print_factorize_list endFlags l = do
-  print_factorize_list l
+threadPrintFactorizeList :: Chan () -> [Int] -> IO ()
+threadPrintFactorizeList endFlags l = do
+  mapM_ (\n -> print $! factorize n) l
   writeChan endFlags ()
-
 
 main :: IO ()
 main = do
   endFlags <- newChan
   numThreads <- getNumCapabilities
-  mapM_ (\l -> forkIO $ thread_print_factorize_list endFlags l) (g numThreads)
+  mapM_ (\l -> forkIO $ threadPrintFactorizeList endFlags l) (g numThreads)
   mapM_ (\_ -> readChan endFlags) [1..numThreads]
