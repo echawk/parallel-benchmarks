77798619bd3d566be4a5674e6b80f06e5e75a438
Ethan <ethan.hawk@valpo.edu>
2022-05-25 15:18:39 -0500

    factorize.lisp: refactor

--- factorization/factorize.lisp
+++ factorization/factorize.lisp
@@ -1,33 +1,19 @@
-(defun n-to-1 (x)
-  "Generates a list from [n] to 1, with a step size of -1."
-  (if (= x 0) '() (cons x (n-to-1 (- x 1)))))
+;; TODO - https://lispcookbook.github.io/cl-cookbook/process.html
 
-(defun make-1-to-n-list (n)
-  "Generates a list from 1 to [n] with a step size of 1."
-  (reverse (n-to-1 n)))
-  
-(defun divides (n m)
-  "Does [n] divide [m]?"
-  (= (mod m n) 0))
-
+;; http://cl-cookbook.sourceforge.net/loop.html
 (defun factorize (n)
-  "Factorize takes a number [n] and returns a list of all of it's factors."
-  (remove-if (lambda (x) (not (divides x n))) (make-1-to-n-list n)))
+  "factorize takes a number [n] and returns a list containing all of its factors."
+  (loop for x from 1 to n
+        when (= (mod n x) 0) ;; When x cleanly divides n, collect it.
+        collect x))
 
+(defun factorize-iota (start inc_size)
+  (print (factorize start))
+  (factorize-iota (+ start inc_size) inc_size))
 
-; Credit:
-; http://dnaeon.github.io/generating-sequences-in-common-lisp/
-(defun iota-generator (&key (start 0) (step 1))
-  (let ((i 0))
-    (lambda ()
-      (prog1 start (incf start step) (incf i)))))
-
-
-(defparameter *iota* (iota-generator))
-
-; Look into using a generator...
+;; TODO - Spin up multiple threads, using a similar technique to the
+;; haskell version of this program.
 (defun main ()
-  (loop
-    (print (factorize (funcall *iota*)))))
+  (factorize-iota 1 1))
 
-;(main)
+(main)
