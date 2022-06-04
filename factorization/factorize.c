cb2096ededb681267fdd9726cb1bb7499c01defc
Ethan <ethan.hawk@valpo.edu>
2022-06-03 22:29:37 -0500

    factorize.c: add MPI support

--- factorization/factorize.c
+++ factorization/factorize.c
@@ -1,18 +1,29 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifdef USE_MPI
+#include <mpi.h>
+#else
+#include <pthread.h>
+#endif
+
 #ifndef CPUS
 #define CPUS 1
 #endif
-
 /*
    To compile:
-   cc -DCPUS=<num_cpu> -lpthread factorize.c
+   cc -DCPUS=<num_cpu> -lpthread factorize.c -o factorize
 
    Or run directly with tcc:
    tcc -DCPUS=<num_cpu> -lpthread -run factorize.c
+
+   To compile (with MPI):
+   mpicc -DUSE_MPI factorize.c -o factorize
+
+   To run (with MPI):
+   mpiexec ./factorize
+
 */
 
 void factorize(int n) {
@@ -26,21 +37,24 @@ void factorize(int n) {
   printf(")\n");
 }
 
-void factorize_start_from(int start) {
+void factorize_start_from(int start, int total_cpus) {
   while (1) {
     factorize(start);
-    start += CPUS;
+    start += total_cpus;
   }
 }
 
 void *thread_fact(void *arg) {
   int *v = (int *)arg;
-  factorize_start_from(v[0]);
+#ifndef USE_MPI
+  factorize_start_from(v[0], CPUS);
+#else
+#endif
   return NULL;
 }
 
 int main() {
-
+#ifndef USE_MPI
   pthread_t pids[CPUS];
 
   int i, start = 0;
@@ -53,6 +67,14 @@ int main() {
   for (i = 0; i < CPUS; i++) {
     pthread_join(pids[i], NULL);
   }
-
+#else
+  int world_size;
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  factorize_start_from(world_rank, world_size);
+  MPI_Finalize();
+#endif
   return 1;
 }
