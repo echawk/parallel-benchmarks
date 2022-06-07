0e2fe1d2ea03df9de4d9078200e1c70d68dd5e68
Ethan <ethan.hawk@valpo.edu>
2022-06-07 13:54:53 -0500

    matrix-mult.hs: finish implementation

--- matrix-mult/matrix-mult.hs
+++ matrix-mult/matrix-mult.hs
@@ -1,29 +1,23 @@
+--ASSUMES ALL MATRICES ARE SQUARE!
 
-
-type Matrix = [[Integer]]
-type Vector =  [Integer]
-
---mult_matr :: Matrix -> Matrix -> Matrix
---mult_matr A B =
+type Matrix = [[Int]]
+type Vector =  [Int]
+type Scalar =   Int
 
 n_in_lst :: Int -> [a] -> a
-n_in_lst n lst = head $ reverse $ take n lst 
+n_in_lst n lst = head $ reverse $ take n lst
 
 col_n_matrix :: Int -> Matrix -> Vector
 col_n_matrix c m = map (n_in_lst c) m
 
-row_n_matrix :: Integer -> Matrix -> Vector
+row_n_matrix :: Int -> Matrix -> Vector
 row_n_matrix 1 m = head m
 row_n_matrix r m = row_n_matrix (r - 1) (tail m)
 
-                   
-ex = [[1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9]]
+row_col_mult :: Vector -> Vector -> Scalar
+row_col_mult row_v col_v = sum $ map (\(a,b) -> a * b) $ zip row_v col_v
 
-ex2 = [[1, 2, 3],
-       [0, 0, 0],
-       [7, 8, 9]]
-
---foldl (+) 0 $ map (\(a,b) -> a * b) $ zip (row_n_matrix 1 ex) (col_n_matrix 1 ex2)
-
+matrix_mult :: Matrix -> Matrix -> Matrix
+matrix_mult m1 m2 =
+    let aux_mult = (\row_v -> map (\x -> row_col_mult row_v (col_n_matrix x m2)) [1..(length m2)]) in
+    map (\x -> aux_mult $ row_n_matrix x m1) [1..(length m1)]
