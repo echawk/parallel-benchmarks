b9c712d0a5adf93e3f016458dbc1cabdde1978f5
Ethan <ethan.hawk@valpo.edu>
2022-06-14 23:17:24 -0500

    monte-carlo-hs: add implementation

--- monte-carlo/monte-carlo-hs/app/Main.hs
+++ monte-carlo/monte-carlo-hs/app/Main.hs
@@ -1,4 +1,21 @@
+
 module Main where
 
+import System.Random
+
+type Pair = (Double, Double)    
+
+monteCarlo :: [Pair] -> Double
+monteCarlo tossesLst =
+    4 * fromIntegral numInCircle / (fromIntegral (length tossesLst))
+    where numInCircle = length $ filter pairInCircle tossesLst 
+ 
+pairInCircle :: Pair -> Bool
+pairInCircle (x, y) = x*x + y*y <= 1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  g <- newStdGen 
+  let totalTosses = 100000
+      lst = take totalTosses $ (randoms g :: [Pair]) in
+    print $ monteCarlo lst 
