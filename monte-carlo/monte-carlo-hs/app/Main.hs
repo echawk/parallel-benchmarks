a8b842c97f05c8dbfbb1b155859e78b805c4ba99
Ethan <ethan.hawk@valpo.edu>
2022-06-15 14:43:00 -0500

    monte-carlo-hs: increase totalTosses & minor optim

--- monte-carlo/monte-carlo-hs/app/Main.hs
+++ monte-carlo/monte-carlo-hs/app/Main.hs
@@ -3,19 +3,19 @@ module Main where
 
 import System.Random
 
-type Pair = (Double, Double)    
+type Pair = (Double, Double)
 
-monteCarlo :: [Pair] -> Double
-monteCarlo tossesLst =
-    4 * fromIntegral numInCircle / (fromIntegral (length tossesLst))
-    where numInCircle = length $ filter pairInCircle tossesLst 
- 
+monteCarlo :: [Pair] -> Int -> Double
+monteCarlo tossesLst tossesNum =
+    4 * fromIntegral numInCircle / (fromIntegral tossesNum)
+    where numInCircle = length $ filter pairInCircle tossesLst
+
 pairInCircle :: Pair -> Bool
 pairInCircle (x, y) = x*x + y*y <= 1
 
 main :: IO ()
 main = do
-  g <- newStdGen 
-  let totalTosses = 100000
+  g <- newStdGen
+  let totalTosses = 10000000
       lst = take totalTosses $ (randoms g :: [Pair]) in
-    print $ monteCarlo lst 
+    print $ monteCarlo lst totalTosses
