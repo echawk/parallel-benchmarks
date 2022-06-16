d9849ef53288fdde9d6f484a757fed415edf9f16
Ethan <ethan.hawk@valpo.edu>
2022-06-16 01:13:15 -0500

    monte-carlo-hs: implement parallel version of algorithm

--- monte-carlo/monte-carlo-hs/app/Main.hs
+++ monte-carlo/monte-carlo-hs/app/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import System.Random
+import Control.Parallel.Strategies
 
 type Pair = (Double, Double)
 
@@ -10,12 +11,28 @@ monteCarlo tossesLst tossesNum =
     4 * fromIntegral numInCircle / (fromIntegral tossesNum)
     where numInCircle = length $ filter pairInCircle tossesLst
 
+chunkList :: Int -> [a] -> [[a]]
+chunkList chunkSize lst =
+    aux chunkSize lst []
+                        
+aux chunkSize lst acc =
+  if chunkSize >= (length lst) then lst : acc
+  else aux chunkSize (drop chunkSize lst) ((take chunkSize lst) : acc)
+
+parMonteCarlo :: [Pair] -> Int -> Int -> Double
+parMonteCarlo tossesLst tossesNum numCpus =
+    let chunks = chunkList numCpus tossesLst 
+        numInCircle = length $ concat (map (\l -> filter pairInCircle l) chunks `using` parList rseq)
+    in 4 * fromIntegral numInCircle / (fromIntegral tossesNum)
+      
 pairInCircle :: Pair -> Bool
 pairInCircle (x, y) = x*x + y*y <= 1
 
 main :: IO ()
 main = do
   g <- newStdGen
-  let totalTosses = 10000000
-      lst = take totalTosses $ (randoms g :: [Pair]) in
-    print $ monteCarlo lst totalTosses
+  let totalTosses = 10000
+      lst = take totalTosses $ (randoms g :: [Pair]) in 
+      do
+          print $ monteCarlo    lst totalTosses
+          print $ parMonteCarlo lst totalTosses 1
