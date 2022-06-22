6c043f95e4de8fdc1d0ba9d06ff16114137a9c1d
Ethan <ethan.hawk@valpo.edu>
2022-06-22 16:59:34 -0500

    primes-hs: create initial impl

--- primes/primes-hs/app/Main.hs
+++ primes/primes-hs/app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Control.Parallel.Strategies
+
+isPrime :: Int -> Bool
+isPrime n = foldl (&&) True $ map (\y -> n `rem` y /= 0) [2..floor(sqrt(fromIntegral n))]
+
+-- Infinite list of all of the primes. Uses the sieve of Eratosthenes in Parallel.
+primes :: [Int]
+primes = filter isPrime [2..] --`using` parListChunk 500 rseq
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn $ show $ take 100 primes
